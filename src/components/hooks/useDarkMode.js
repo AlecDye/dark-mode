8900932bfa5b819cc09cc356309e4c63e9b71f3c
Alec Dye <alecdye@gmail.com>
2020-02-12 15:09:11 -0800

    adding logic to dark mode hook

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -2,10 +2,15 @@ import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
 const useDarkMode = key => {
-    const [light, setLight] = useLocalStorage(key);
+    const [theme, setTheme] = useLocalStorage(key);
 
     // check value from useLocalStorage if true/false
     useEffect(() => {
-
-    })
+        if (useLocalStorage(theme) === true) {
+            return document.body.classList.add("dark-mode")
+        } else {
+            return document.body.classList.remove("dark-mode")
+        }
+    }, [theme])
+    return [theme, setTheme]
 }
